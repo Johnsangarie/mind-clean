9310492dd682d9eeec266e9818822d0268516373
John Sangarie <sangariej@berea.edu>
2025-03-18 00:32:37 -0400

    ready to build!

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MainView } from './components/MainView'
 
 function App() {
- 
 
   return (
     <>
-     
-      <h1>Vite + React</h1>
+     <MainView/>
+      
     
     </>
   )
---
+++ src/components/MainView.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+
+export function  MainView()  {
+       return <Button> Submit</Button>
+    
+    
+}
